1f0e1b913f8a6601603a41152071eb3c827be7a4
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-07 16:10:29 -0300

    fix: add test suite

--- src/pages/Details/Details.test.jsx
+++ src/pages/Details/Details.test.jsx
@@ -1,3 +1,7 @@
 export const Details = () => {
   return <h1>Details</h1>;
 }
+
+describe("<Details />", () => {
+  it("", () => {})
+});
--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -1,3 +1,7 @@
 export const Home = () => {
   return <h1>Home</h1>;
 }
+
+describe("<Home />", () => {
+  it("", () => {})
+});
