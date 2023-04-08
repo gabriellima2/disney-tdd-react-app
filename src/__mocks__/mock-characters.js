6f36aed09beba897b5eead11eaad1fc0bd0158be
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-08 13:55:08 -0300

    feat: add home page content

---
+++ src/__mocks__/mock-characters.js
@@ -0,0 +1,7 @@
+export const mockCharacters = [
+  {
+    _id: "01",
+    name: "any_name",
+    imageUrl: "any_image_url",
+  }
+] 
--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -1,7 +1,57 @@
-export const Home = () => {
-  return <h1>Home</h1>;
+import { render, screen, waitFor } from "@testing-library/react";
+import { useFetch } from "../../hooks/use-fetch";
+import { mockError } from "../../__mocks__/mock-error";
+import { mockCharacters } from "../../__mocks__/mock-characters";
+
+export const Home = (props) => {
+  const { service } = props;
+  const { isError, isLoading, error, data } = useFetch(service);
+  if (isLoading) return <h1 data-testid="loading"></h1>;
+  if (isError) return <h1 data-testid="error">{error.message}</h1>
+  return (
+    <main data-testid="content">
+      <ul>
+        {data.map((character) => <li>{character.name}</li>)}
+      </ul>
+    </main>
+  );
 }
 
+const characterService = vitest.fn();
+const renderComponent = () => render(<Home service={characterService} />);
+
+vitest.mock("../../hooks/use-fetch");
+
 describe("<Home />", () => {
-  it("", () => {})
+  describe("Render", () => {
+    it("should render loading", () => {
+      useFetch.mockReturnValue({ isLoading: true, isError: false, error: null, data: null });
+      renderComponent();
+      
+      expect(screen.getByTestId("loading")).toBeInTheDocument();
+      expect(screen.queryByTestId("error")).not.toBeInTheDocument();
+      expect(screen.queryByTestId("content")).not.toBeInTheDocument();
+    });
+    it("should render error message", () => {
+      useFetch.mockReturnValue({ isLoading: false, isError: true, error: mockError, data: null });
+      renderComponent();
+
+      expect(screen.queryByTestId("loading")).not.toBeInTheDocument();
+      expect(screen.getByTestId("error")).toBeInTheDocument();
+      expect(screen.getByText(mockError.message)).toBeInTheDocument();
+      expect(screen.queryByTestId("content")).not.toBeInTheDocument();
+    });
+    it("should render character list", () => {
+      useFetch.mockReturnValue({ isLoading: false, isError: false, error: null, data: mockCharacters });
+      renderComponent();
+
+      expect(screen.queryByTestId("loading")).not.toBeInTheDocument();
+      expect(screen.queryByTestId("error")).not.toBeInTheDocument();
+      expect(screen.getByTestId("content")).toBeInTheDocument();
+      mockCharacters.forEach((character) => {
+        const { name } = character;
+        expect(screen.getByText(name)).toBeInTheDocument();
+      })
+    })
+  });
 });
