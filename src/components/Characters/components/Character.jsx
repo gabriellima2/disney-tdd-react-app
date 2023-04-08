56cf114ce23ec6c3ff4666320d3810d0ec92a1d2
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-08 12:49:24 -0300

    feat: add link directing to details page in character component

--- src/components/Characters/components/Character.jsx
+++ src/components/Characters/components/Character.jsx
@@ -1,11 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 export const Character = (props) => {
-  const { _id, image, name } = props;
+  const { id, image, name } = props;
   return (
     <li>
-      <a>
+      <NavLink to={`/details/${id}`} title={`Ver detalhes de ${name}`}>
         <img src={image.src} alt={image.alt} />
         <h1>{name}</h1>
-      </a>
+      </NavLink>
     </li>
   );
 }
--- src/components/Characters/components/Character.test.jsx
+++ src/components/Characters/components/Character.test.jsx
@@ -1,22 +1,44 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from "@testing-library/user-event";
 
 import { Character } from ".";
 import { mockCharacter } from "../mocks/mock-character";
 
 const renderComponent = (props) => render(
+  <BrowserRouter>
     <Character {...props} />
+  </BrowserRouter>  
 );
+
+const makeTitle = (name) => `Ver detalhes de ${name}`;
+const makeDetailsUrl = (id) => `/details/${id}`
 
 describe("<Character />", () => {
   describe("Render", () => {
     it("should render correctly", () => {
       renderComponent(mockCharacter);
 
+      const link = screen.getByTitle(makeTitle(mockCharacter.name));
       const image = screen.getByAltText(mockCharacter.image.alt);
 
+      expect(link).toBeInTheDocument();
       expect(image).toBeInTheDocument();
+      expect(link).toHaveAttribute("href", makeDetailsUrl(mockCharacter.id));
       expect(image).toHaveAttribute("src", mockCharacter.image.src);
       expect(screen.getByText(mockCharacter.name)).toBeInTheDocument();
     });
   });
+  describe("Interactions", () => {
+    describe("Click", () => {
+      it("should navigate to details page with character id when clicked", async () => {
+        renderComponent(mockCharacter);
+
+        const link = screen.getByTitle(makeTitle(mockCharacter.name));
+        await userEvent.click(link);
+
+        expect(window.location.pathname).toBe(makeDetailsUrl(mockCharacter.id));
+      })
+    })
+  })
 });
