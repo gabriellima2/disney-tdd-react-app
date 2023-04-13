7e4cf9dd46ffe99b8fccce10737b5fbc3af5efe7
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-04-13 14:48:24 -0300

    refactor: add infos validation of additional-infos component

--- src/components/CharacterInfo/components/AdditionalInfos.jsx
+++ src/components/CharacterInfo/components/AdditionalInfos.jsx
@@ -1,11 +1,16 @@
 export const AdditionalInfos = (props) => {
   const { title, infos } = props;
+  const hasInfos = infos.length > 0;
   return (
     <section>
       <h2>{title}</h2>
-      <ul>
-        {infos.map((info) => <li key={info}>{info}</li>)}
-      </ul>
+      {hasInfos ? (
+        <ul>
+          {infos.map((info) => <li key={info}>{info}</li>)}
+        </ul>
+      ) : (
+        <small>Sem informações!</small>
+      )}
     </section>
   );
 }
--- src/components/CharacterInfo/components/AdditionalInfos.test.jsx
+++ src/components/CharacterInfo/components/AdditionalInfos.test.jsx
@@ -4,17 +4,25 @@ import { AdditionalInfos } from ".";
 import { mockInfos } from "../mocks/mock-infos";
 
 const TITLE = "any_title";
-const renderComponent = () => render(<AdditionalInfos title={TITLE} infos={mockInfos} />);
+const WITHOUT_INFOS_MESSAGE = "Sem informações!";
+
+const renderComponent = (props) => render(<AdditionalInfos title={TITLE} infos={props.infos} />);
 
 describe("<AdditionalInfos />", () => {
   describe("Render", () => {
-    it("should render correctly", () => {
-      renderComponent();
+    it("should render correctly with infos", () => {
+      renderComponent({ infos: mockInfos });
 
       expect(screen.getByText(TITLE)).toBeInTheDocument();
       mockInfos.forEach((info) => {
         expect(screen.getByText(info)).toBeInTheDocument();
       });
+    })
+    it("should render correctly without infos", () => {
+      renderComponent({ infos: [] });
+
+      expect(screen.getByText(TITLE)).toBeInTheDocument();
+      expect(screen.getByText(WITHOUT_INFOS_MESSAGE)).toBeInTheDocument();
     })
   });
 });
